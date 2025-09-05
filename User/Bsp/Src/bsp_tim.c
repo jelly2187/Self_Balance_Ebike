ca4541b4fde1d0de1f45a0f45d91d3ea7113e6d8
SuperFat <superfat219@gmail.com>
2025-09-05 19:06:50 +0800

    add the CAN Encoder for XAG A25 Motor.

--- User/Bsp/Src/bsp_tim.c
+++ User/Bsp/Src/bsp_tim.c
@@ -7,6 +7,7 @@
 #include "balance_control.h"
 #include "DJI_Motor.h"
 #include "DM_Motor.h"
+#include "Encoder.h"
 #include "INS.h"
 #include "pid.h"
 #include "stm32h7xx_hal_tim.h"
@@ -14,15 +15,18 @@
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
     // 确保是我们的PID控制定时器(TIM2)触发的中断
     if (htim->Instance == TIM2) {
-        // 1. 计算PID输出
-        float pid_output = Motor_PID_Calculate(&dji_motor_speed_pid, dji_target_speed_rpm,
-                                               (float) motor_feedback[0].speed_rpm);
+        // // 1. 计算PID输出
+        // float pid_output = Motor_PID_Calculate(&dji_motor_speed_pid, dji_target_speed_rpm,
+        //                                        (float) motor_feedback[0].speed_rpm);
+        //
+        // // 2. 将PID输出作为电流指令发送给电机
+        // DJI_Motor_SendCommand((int16_t) pid_output);
 
-        // 2. 将PID输出作为电流指令发送给电机
-        DJI_Motor_SendCommand((int16_t) pid_output);
+        // Update Encoder
     }
     if (htim->Instance == TIM4) {
         INS_Update(); // 在这里以1kHz的频率调用姿态更新
+        Encoder_Update(0.001f);
         // printf("")
 #if AUTO_SYSTEM_MODE
         dm_target_position_rad = Balance_Controller_Update(INS_Info.Angle[IMU_ANGLE_INDEX_ROLL],
